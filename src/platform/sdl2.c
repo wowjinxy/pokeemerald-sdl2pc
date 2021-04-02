143459307d19d11f068cc5d9eef4029260bc05a9
JaceCear <11230293+JaceCear@users.noreply.github.com>
2021-04-02 19:04:17 +0200

    Fix keyboard input being ignored

--- src/platform/sdl2.c
+++ src/platform/sdl2.c
@@ -308,43 +308,46 @@ u16 GetXInputKeys()
 	ZeroMemory(&state, sizeof(XINPUT_STATE));
 
 	DWORD dwResult = XInputGetState(0, &state);
-	u16 _keys = 0;
+	u16 xinputKeys = 0;
 
-	if (dwResult == ERROR_SUCCESS) {
-		/* A */      _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_A) >> 12;
-		/* B */      _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_X) >> 13;
-		/* Start */  _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_START) >> 1;
-		/* Select */ _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_BACK) >> 3;
-		/* L */      _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER) << 1;
-		/* R */      _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) >> 1;
-		/* Up */     _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP) << 6;
-		/* Down */   _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN) << 6;
-		/* Left */   _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT) << 3;
-		/* Right */  _keys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT) << 1;
+	if (dwResult == ERROR_SUCCESS)
+	{
+		/* A */      xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_A) >> 12;
+		/* B */      xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_X) >> 13;
+		/* Start */  xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_START) >> 1;
+		/* Select */ xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_BACK) >> 3;
+		/* L */      xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER) << 1;
+		/* R */      xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) >> 1;
+		/* Up */     xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP) << 6;
+		/* Down */   xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN) << 6;
+		/* Left */   xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT) << 3;
+		/* Right */  xinputKeys |= (state.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT) << 1;
 
 
 		/* Control Stick */
 		float xAxis = (float)state.Gamepad.sThumbLX / (float)SHRT_MAX;
 		float yAxis = (float)state.Gamepad.sThumbLY / (float)SHRT_MAX;
 
-		if (xAxis < -STICK_THRESHOLD) _keys |= DPAD_LEFT;
-		if (xAxis > STICK_THRESHOLD)  _keys |= DPAD_RIGHT;
-		if (yAxis < -STICK_THRESHOLD) _keys |= DPAD_DOWN;
-		if (yAxis > STICK_THRESHOLD)  _keys |= DPAD_UP;
+		if (xAxis < -STICK_THRESHOLD) xinputKeys |= DPAD_LEFT;
+		if (xAxis >  STICK_THRESHOLD) xinputKeys |= DPAD_RIGHT;
+		if (yAxis < -STICK_THRESHOLD) xinputKeys |= DPAD_DOWN;
+		if (yAxis >  STICK_THRESHOLD) xinputKeys |= DPAD_UP;
 
 
 		/* Speedup */
-		timeScale = (state.Gamepad.bRightTrigger > 0x80) ? 5.0 : 1.0;
+		// Note: 'speedup' variable is only (un)set on keyboard input
+		timeScale = (state.Gamepad.bRightTrigger > 0x80 || speedUp) ? 5.0 : 1.0;
 	}
 
-	return _keys;
+	return xinputKeys;
 }
-#endif // _WIN32"
+#endif // _WIN32
 
 u16 Platform_GetKeyInput(void)
 {
 #ifdef _WIN32
-	keys = GetXInputKeys();
+	u16 gamepadKeys = GetXInputKeys();
+	return (gamepadKeys != 0) ? gamepadKeys : keys;
 #endif
 
     return keys;
